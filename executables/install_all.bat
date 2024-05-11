14430a0e0b53c1af68b0f25961593253c51706d5
AbdoWise-z <moh2002.abdow@gmail.com>
2024-05-12 01:04:21 +0300

    executables

---
+++ executables/install_all.bat
@@ -0,0 +1,9 @@
+echo off
+cls
+pip install opencv-contrib-python
+pip install xgboost
+pip install scikit-learn
+pip install matplotlib
+pip install Flask
+pip install requests
+echo Done
---
+++ executables/run_server.bat
@@ -0,0 +1,5 @@
+echo off
+cls
+cd..
+cd server
+python server.py
